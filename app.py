aac29643a93531b6bc5f7d0fd7069dc8881bb99c
aristocrat71 <sheth.mitul.71@gmail.com>
2024-12-19 12:22:26 +0530

    Static signup page created

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def redirect_login():
 def login():
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
