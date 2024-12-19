ba177740215f342d795d44f799ff179061894c00
aristocrat71 <sheth.mitul.71@gmail.com>
2024-12-19 12:16:08 +0530

    static login page created

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-import Flask
+from flask import Flask, render_template
+from flask import redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def redirect_login():
+    return redirect('/login')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
