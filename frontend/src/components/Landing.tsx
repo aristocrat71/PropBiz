756555e6d42d1ae84936329d99d98c82b1408c27
aristocrat71 <sheth.mitul.71@gmail.com>
2025-07-02 10:48:34 +0530

    Update

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,16 +1,23 @@
-import buildingImg from '../assets/react.svg';
 import './Landing.css';
 
 const Landing = () => (
-  <main className="landing-main">
-    <div className="landing-content">
-      <h1>Welcome to PropBiz</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nisi eu consectetur consectetur, nisl nisi consectetur nisi, euismod euismod nisi nisi euismod.</p>
+  <div className="landing-container">
+    <div className="landing-left">
+      <h1>Lorem ipsum</h1>
+      <div className="landing-text">
+        asdawdasdawdsdawdasdasd<br />
+        awdasdasd<br />
+        awdasdasdasdasdad
+      </div>
     </div>
-    <div className="landing-image">
-      <img src={buildingImg} alt="Buildings" />
+    <div className="landing-right">
+      <svg width="220" height="220" viewBox="0 0 220 220" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="15" y="15" width="190" height="190" rx="20" stroke="#222" strokeWidth="6" fill="none"/>
+        <circle cx="60" cy="60" r="16" stroke="#222" strokeWidth="6" fill="none"/>
+        <polyline points="40,170 120,100 180,170" stroke="#222" strokeWidth="6" fill="none"/>
+      </svg>
     </div>
-  </main>
+  </div>
 );
 
 export default Landing; 
